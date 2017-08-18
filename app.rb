7e4aedea281f8fe9323cf8b4893c1925493edf2c
neodevelop <juan@makingdevs.com>
2017-08-18 12:08:04 -0500

    Creando algunas rutas con Sinatra

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+get '/frank-says' do
+  'Put this in your pipe & smoke it!'
+end
+
+get '/' do
+  @message = params['error']
+  erb :index
+end
+
+post '/Otra/contact/:p' do
+  puts "#{params}"
+  @saludo = "Hola "
+  @nombres = "#{params['p']} #{params['name']}"
+  if params['name'].empty?
+    redirect "/?error=esta vacio"
+  else
+    erb :Otra
+  end
+end 
+
+
