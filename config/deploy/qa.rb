cd0580d9d4d8135033959b18be44ebe9677dfcee
Vanessa <vanvanemily@gmail.com>
2017-08-24 10:08:17 -0500

    #3 Setting deply file for qa

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+ server "qa-ienpop.barist.coffee", user: "ec2-user", roles: %w{app web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
