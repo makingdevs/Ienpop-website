ee971b0e8ad888100b2ff1d50e9d12f058396eff
Vanessa <vanvanemily@gmail.com>
2017-08-28 15:10:08 -0500

    #3 Adding class into app rb

--- app.rb
+++ app.rb
@@ -1,33 +1,39 @@
 require 'sinatra'
 
-get '/frank-says' do
-  'Put this in your pipe & smoke it!'
-end
+class IENPOP < Sinatra::Base
+  get '/frank-says' do
+    'Put this in your pipe & smoke it!'
+  end
 
-get '/' do
-  @message = params['error']
-  erb :index
-end
+  get '/' do
+    @message = params['error']
+    erb :index
+  end
 
-post '/Otra/contact/:p' do
-  puts "#{params}"
-  @saludo = "Hola "
-  @nombres = "#{params['p']} #{params['name']}"
-  if params['name'].empty?
-    redirect "/?error=esta vacio"
-  else
-    erb :Otra
+  post '/Otra/contact/:p' do
+    puts "#{params}"
+    @saludo = "Hola "
+    @nombres = "#{params['p']} #{params['name']}"
+    if params['name'].empty?
+      redirect "/?error=esta vacio"
+    else
+      erb :Otra
+    end
+  end
+
+  get '/home' do
+    erb :index
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/contact' do
+    erb :contact
   end
 end
 
-get '/home' do 
-  erb :index
-end
-
-get '/about' do
-  erb :about 
-end
-
-get '/contact' do
-  erb :contact
+if __FILE__ == $0
+    IENPOP.run!
 end
