d870e874b928ceec6f2f0d5817ec8901a40cee4b
Vanessa <vanvanemily@gmail.com>
2017-08-30 16:57:30 -0500

    #3 Adding new model directory which save all models classes

---
+++ lib/sinatra/models/course.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+class Course
+  attr_accessor :active
+  attr_accessor :duration
+  attr_accessor :name
+
+  @id
+  @version
+  @active
+  @key
+  @dateCreated
+  @duration
+  @lastUpdated
+  @notebook
+  @name
+  @english
+  @description
+  @englishName
+end
+
