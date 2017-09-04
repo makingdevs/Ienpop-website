a6fa8de60e1cc5f3f6a4aac06e6d1b4636441bfd
Vanessa <vanvanemily@gmail.com>
2017-09-04 12:12:52 -0500

    #3 Adding three queries for each kind of notebook which could be a, b, c or d

--- lib/sinatra/course_manager.rb
+++ lib/sinatra/course_manager.rb
@@ -1,19 +1,63 @@
 require 'mysql2'
 require 'sinatra'
-require './lib/sinatra/Connection'
+require './lib/sinatra/db_manager'
 require './lib/sinatra/models/course'
 
 class CourseManager
 
   def initialize
-    @conn = Connection.new
+    @conn = DBManager.new
   end
 
   def list
     courses = []
-    conn.executeQuery("Select * from curso").each do |row|
+    @conn.execute_query("Select * from curso").each do |row|
       course = Course.new
       course.active = row['clave'],
+      course.duration = row['duracion'],
+      course.name = row['nombre']
+      courses << course
+   end
+    courses
+  end
+
+  def showCoursesNotebookA
+    courses = []
+    @conn.execute_query("Select * from curso where libreta='A' and activo=1").each do |row|
+      course = Course.new
+      course.duration = row['duracion'],
+      course.name = row['nombre'],
+      courses << course
+    end
+    courses
+  end
+
+  def showCoursesNotebookB
+   courses = []
+    @conn.execute_query("Select duracion, nombre  from curso where libreta='B' and activo=1").each do |row|
+      course = Course.new
+      course.duration = row['duracion'],
+      course.name = row['nombre']
+      courses << course
+    end
+    courses
+  end
+
+  def showCoursesNotebookC
+    courses = []
+    @conn.execute_query("Select duracion, nombre  from curso where libreta='C' and activo=1").each do |row|
+      course = Course.new
+      course.duration = row['duracion'],
+      course.name = row['nombre']
+      courses << course
+    end
+    courses
+  end
+
+  def showCoursesNotebookD
+    courses = []
+    @conn.execute_query("Select duracion, nombre  from curso where libreta='D' and activo=1").each do |row|
+      course = Course.new
       course.duration = row['duracion'],
       course.name = row['nombre']
       courses << course
