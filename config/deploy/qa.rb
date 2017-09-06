1ad110121cf87e63c280553bd90e2c6a694977a6
neodevelop <juan@makingdevs.com>
2017-09-05 23:51:29 -0500

    #6 setup branch for test

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
- server "ienpop-qa.makingdevs.com", user: "ec2-user", roles: %w{app web}
+server "ienpop-qa.makingdevs.com", user: "ec2-user", roles: %w{app web}
+set :branch, "feature/6"
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
