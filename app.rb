9624d5a881b32819fdf355040a249e362acd539e
Vanessa <vanvanemily@gmail.com>
2017-09-08 08:47:57 -0500

    #9 Removing methods that are not required

--- app.rb
+++ app.rb
@@ -4,24 +4,10 @@ require './lib/sinatra/course_manager'
 class IENPOP < Sinatra::Base
 
   course_manager = CourseManager.new
-  get '/frank-says' do
-    'Put this in your pipe & smoke it!'
-  end
 
   get '/' do
     @message = params['error']
     erb :index
-  end
-
-  post '/Otra/contact/:p' do
-    puts "#{params}"
-    @saludo = "Hola "
-    @nombres = "#{params['p']} #{params['name']}"
-    if params['name'].empty?
-      redirect "/?error=esta vacio"
-    else
-      erb :Otra
-    end
   end
 
   get '/home' do
