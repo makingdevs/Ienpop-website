2e9332b7795977d38643427234e6d1cd07f9c1dc
Vanessa <vanvanemily@gmail.com>
2017-09-13 16:15:00 -0500

    #30 Adding new method which list all managers from json file

--- lib/ienpop/managers_manager.rb
+++ lib/ienpop/managers_manager.rb
@@ -7,6 +7,11 @@ class ManagersManager
   def initialize
     uri = URI.parse("http://training.makingdevs.com.s3.amazonaws.com/manager.json")
     @content = Net::HTTP.get_response uri
+
+  end
+
+  def list_all_manager
+    values = JSON.parse(@content.body)
   end
 
 
