8176e48aad145356dd130dac33c56af977ef92fb
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-09-15 15:34:33 -0500

    #29 Inserting encargado en sedes

--- lib/ienpop/models/sede.rb
+++ lib/ienpop/models/sede.rb
@@ -1,10 +1,12 @@
 class Sede
+  attr_accessor :encargado
   attr_accessor :name
   attr_accessor :address
   attr_accessor :phone
   attr_accessor :email
   attr_accessor :coordinates
 
+  @encargado
   @name
   @address
   @phone
--- lib/ienpop/sedes_manager.rb
+++ lib/ienpop/sedes_manager.rb
@@ -6,15 +6,18 @@ require 'ostruct'
 class SedesManager 
 
 	def initialize 
-		uri = URI.parse("http://training.makingdevs.com.s3.amazonaws.com/data.json")
-		content = Net::HTTP.get_response uri
-		@values = JSON.parse(content.body)
+		#uri = URI.parse("http://training.makingdevs.com.s3.amazonaws.com/data.json")
+		#content = Net::HTTP.get_response uri
+		#@values = JSON.parse(content.body)
+		json = File.read("./lib/ienpop/data.json")
+        @values = JSON.parse(json, object_class: OpenStruct)
 	end
 
 	def list_all_sedes
 		sedes = []
 		@values.sedes.each do |c|
 			sede = Sede.new
+			sede.encargado = c.encargado
 			sede.name = c.name
 			sede.address = c.address
 			sede.phone = c.phone
