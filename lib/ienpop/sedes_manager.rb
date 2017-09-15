41794e92bdbd32dda18c631169e03bcf9ace15b8
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-09-15 12:48:07 -0500

    #28 Creating class SedesManager

---
+++ lib/ienpop/sedes_manager.rb
@@ -0,0 +1,27 @@
+require './lib/ienpop/models/sede'
+require 'json'
+require 'net/http'
+require 'ostruct'
+
+class SedesManager 
+
+	def initialize 
+		uri = URI.parse("http://training.makingdevs.com.s3.amazonaws.com/data.json")
+		content = Net::HTTP.get_response uri
+		@values = JSON.parse(content.body)
+	end
+
+	def list_all_sedes
+		sedes = []
+		@values.sedes.each do |c|
+			sede = Sede.new
+			sede.name = c.name
+			sede.address = c.address
+			sede.phone = c.phone
+			sede.email = c.email
+			sede.coordinates = c.coordinates
+			sedes << sede
+		end
+	sedes
+	end
+end
