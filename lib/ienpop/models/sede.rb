6a92b0b428e1563be54f531c00e6d1e952638b2c
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-09-15 09:58:25 -0500

    #28 Creating and inserting sede.rb at models

---
+++ lib/ienpop/models/sede.rb
@@ -0,0 +1,14 @@
+class sede
+  attr_accessor :name
+  attr_accessor :address
+  attr_accessor :phone
+  attr_accessor :email
+  attr_accessor :coordinates
+
+  @name
+  @address
+  @phone
+  @email
+  @coordinates
+end
+
