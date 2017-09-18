725dfa6106a974848f80dd58e0dd1b5c1d7d4235
Vanessa <vanvanemily@gmail.com>
2017-09-18 10:38:03 -0500

    #34 Receiving params and adding instance attributes

--- lib/ienpop/email_manager.rb
+++ lib/ienpop/email_manager.rb
@@ -1,11 +1,19 @@
 
 class EmailManager
 
-	def receive_info(name, email, subject, message)
-		puts name
-		puts email
-		puts subject
-		puts message
+	def receive_info(params)
+		puts "Estoy en la chase EmailManager"
+		puts "#{params['name']}"
+		puts "#{params['email']}"
+		puts "#{params['subject']}"
+		puts "#{params['message']}"
+		puts "#{params['sedes']}"
+		@name = params['name']
+		@email = params['email']
+		@subject = params['subject']
+		@message = params['message']
+		@sedes = params['sedes']
+
 	end
 end
 
