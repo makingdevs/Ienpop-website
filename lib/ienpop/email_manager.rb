aa13bc7b1b437238b297b752a2d420c90abf40a7
Vanessa <vanvanemily@gmail.com>
2017-09-18 10:42:34 -0500

    #34 Removing params outputs

--- lib/ienpop/email_manager.rb
+++ lib/ienpop/email_manager.rb
@@ -2,18 +2,12 @@
 class EmailManager
 
 	def receive_info(params)
-		puts "Estoy en la chase EmailManager"
-		puts "#{params['name']}"
-		puts "#{params['email']}"
-		puts "#{params['subject']}"
-		puts "#{params['message']}"
-		puts "#{params['sedes']}"
 		@name = params['name']
 		@email = params['email']
 		@subject = params['subject']
 		@message = params['message']
 		@sedes = params['sedes']
-
 	end
+
 end
 
