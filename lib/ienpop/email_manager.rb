e542cf5c85ac173e47cb519be8995df1412528a7
vaneEmy <vanvanemily@gmail.com>
2017-09-22 21:28:02 -0500

    #35 Adding class methods from Mail, reading environments variables in order to send emails

--- lib/ienpop/email_manager.rb
+++ lib/ienpop/email_manager.rb
@@ -1,13 +1,36 @@
+require 'mail'
+require 'net/smtp'
 
-class EmailManager
+class EmailManager 
+  
+  options = { 
+    :address        => ENV["ADDRESS_MAIl"],
+    :port           => ENV["PORT_MAIL"],
+    :authentication => ENV["AUTHENTICATION"],
+    :user_name      => ENV["USER_NAME"],
+    :password       => ENV["PASSWORD_MAIL"],
+    :enable_starttls_auto => ENV["ENABLE_STARTTLS_AUTO"] 
+  }
 
-	def receive_info(params)
+  Mail.defaults do
+    delivery_method :smtp, options
+  end
+
+  Mail.deliver do
+      to 'vanessa@makingdevs.com'
+      from ENV["USER_NAME"]
+      subject 'Sending EMAIl'
+      body 'Pruebita 3'
+  end
+
+  def receive_info(params)
 		@name = params['name']
 		@email = params['email']
 		@subject = params['subject']
 		@message = params['message']
 		@sedes = params['sedes']
 	end
+
 
 end
 
