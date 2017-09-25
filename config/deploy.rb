2827731479b48931997e845841d98a0c3e2564a4
Vanessa <vanvanemily@gmail.com>
2017-09-25 18:09:19 -0500

    #17 Vinculando en el deploy el archivo de configuración

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rbenv_ruby, '2.4.0'
 
 set :puma_user, fetch(:user)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/config.yml')
 
 namespace :bower do
 
